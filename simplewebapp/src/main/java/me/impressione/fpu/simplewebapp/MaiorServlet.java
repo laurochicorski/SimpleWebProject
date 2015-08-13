62b3e5e6a245cc81c155d96bafae1d93586cb8ad
lauro <lauro@laurodell>
2015-08-13 12:44:48 -0300

    Segue arquivos

---
+++ simplewebapp/src/main/java/me/impressione/fpu/simplewebapp/MaiorServlet.java
@@ -0,0 +1,36 @@
+package me.impressione.fpu.simplewebapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/maior")
+public class MaiorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int x = Integer.parseInt(req.getParameter("x"));
+		int y = Integer.parseInt(req.getParameter("y"));
+		PrintWriter writer = resp.getWriter();
+		if(y == x){
+			writer.write(String.format("Os números são iguais"));
+		}else if(x > y){
+			writer.write(String.format("O número " + x + " é maior que " + y));
+		}else{
+			writer.write(String.format("O número " + y + " é maior que " + x));
+		}
+//		if(y > x){
+//			writer.write(String.format("Não foi possível realizar a divisão, pois o valor " + y + " é maior que " + x + "."));
+//		}else if(y == 0){
+//			writer.write(String.format("Não foi possível realizar uma divisão por 0."));
+//		}else{
+//			writer.write(String.format("O resultado desta divisão é "+ x / y));
+//		}
+	}
+}
