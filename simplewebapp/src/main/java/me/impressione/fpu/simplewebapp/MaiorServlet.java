51a43f756aedbfa4d18f95d942dc4b9b8f172282
lauro <lauro@laurodell>
2015-08-13 21:59:24 -0300

    Segue

--- simplewebapp/src/main/java/me/impressione/fpu/simplewebapp/MaiorServlet.java
+++ simplewebapp/src/main/java/me/impressione/fpu/simplewebapp/MaiorServlet.java
@@ -12,25 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/maior") 
 public class MaiorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+  
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int x = Integer.parseInt(req.getParameter("x"));
-		int y = Integer.parseInt(req.getParameter("y"));
 		PrintWriter writer = resp.getWriter();
-		if(y == x){
-			writer.write(String.format("Os números são iguais"));
-		}else if(x > y){
-			writer.write(String.format("O número " + x + " é maior que " + y));
+		if(req.getParameter("x") == "" || req.getParameter("y") == ""){
+			writer.write(String.format("Preenche os dois números"));
+			
 		}else{
-			writer.write(String.format("O número " + y + " é maior que " + x));
+			int x = Integer.parseInt(req.getParameter("x"));
+			int y = Integer.parseInt(req.getParameter("y"));
+			if(y == x){
+				writer.write(String.format("Os números são iguais"));
+			}else if(x > y){
+				writer.write(String.format("O número " + x + " é maior que " + y));
+			}else{
+				writer.write(String.format("O número " + y + " é maior que " + x));
+			}
 		}
-//		if(y > x){
-//			writer.write(String.format("Não foi possível realizar a divisão, pois o valor " + y + " é maior que " + x + "."));
-//		}else if(y == 0){
-//			writer.write(String.format("Não foi possível realizar uma divisão por 0."));
-//		}else{
-//			writer.write(String.format("O resultado desta divisão é "+ x / y));
-//		}
 	}
 }
--- simplewebapp/src/main/java/me/impressione/fpu/simplewebapp/SomaServlet.java
+++ simplewebapp/src/main/java/me/impressione/fpu/simplewebapp/SomaServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/soma")
 public class SomaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+ 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String xstr = req.getParameter("x");
